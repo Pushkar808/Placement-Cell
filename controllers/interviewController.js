f32d1c52ef04e8a8b7dd4cba6d89b7e707a1f744
Pushkar gupta <pushkargupta808@gmail.com>
2022-10-19 22:45:07 +0530

    added to edit or view the student result

--- controllers/interviewController.js
+++ controllers/interviewController.js
@@ -1,25 +1,35 @@
 const interviewSchema = require('../models/interview')
-const ResultSchema=require('../models/results');
-module.exports.addInterview = (req, res) => {
-    interviewSchema.create({//get all data from body as method is post
+const ResultSchema = require('../models/results')
+module.exports.addInterview = async (req, res) => {
+
+    const student_list = req.body.student_list;
+    const interviewData = await interviewSchema.create({//get all data from body as method is post
         title: req.body.title,
         date: req.body.date,
         description: req.body.description,
-        student_mapped: req.body.student_list
-    }, (err, data) => {
-        if (err) {
-            console.log("Some error occured while putting data for student: " + err);
-            return;
-        }
-        res.redirect('back');
+        student_mapped: student_list
     });
+    if (interviewData) { 
+        //if we have interview data the initially we can set result of all student to hold
+        for (let i=0;i<student_list.length;i++) {
+            await ResultSchema.create({
+                interview_id:interviewData.id,
+                student_id:student_list[i],
+                result:"On hold"//setting intially data to hold
+            });
+        }
+        return res.redirect('back');
+    }
 }
 
-module.exports.showInterview=(req,res)=>{
-    interviewSchema.findById(req.query.number).populate('student_mapped').exec((err,data)=>{
-        console.log(data);
-        res.render('interview_explore',{
-            data:data
+module.exports.showInterview = async (req, res) => {//show the interview and result data to the user
+    const interview_data = await interviewSchema.findById(req.query.number).populate('student_mapped');
+    ResultSchema.find({ interview_id:req.query.number }, (err, data) => {
+        // console.log(data);
+        res.render('interview_explore', {
+            data: interview_data,//sending interview schema data
+            result: data //sending result
         })
     })
+
 }
--- models/results.js
+++ models/results.js
@@ -1,19 +1,19 @@
-const mongoose=require('mongoose');
-const resultSchema=mongoose.Schema({//structure for student db schema
-    student_id:{
-        type:String,
-        ref:'Student',
-        required:true
+const mongoose = require('mongoose');
+const resultSchema = mongoose.Schema({//structure for student db schema
+    student_id: {
+        type: String,
+        ref: 'Student',
+        required: true
     },
-    interview_id:{
-        type:String,
-        ref:'Interview',
-        required:true
+    interview_id: {
+        type: String,
+        ref: 'Interview',
+        required: true
     },
-    result:{
-        type:String,
-        required:true
+    result: {
+        type: String,
+        required: true
     }
 });
-const Result=mongoose.model('Result',resultSchema);//creating DB named as "Result"
-module.exports=Result;
+const Result = mongoose.model('Result', resultSchema);//creating DB named as "Result"
+module.exports = Result;

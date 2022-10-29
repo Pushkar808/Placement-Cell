7427494cc7c3898cef1453071846ba2788a526d2
Pushkar gupta <pushkargupta808@gmail.com>
2022-10-29 15:34:11 +0530

    added for deployement

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,5 +1,7 @@
 const mongoose=require('mongoose');
-mongoose.connect("mongodb://localhost:27017/PlacementCell");
+// mongoose.connect("mongodb://localhost:27017/PlacementCell");
+
+mongoose.connect("mongodb+srv://pushkar:EWY4RKwBpB8txyqx@cluster0.1hfkic2.mongodb.net/?retryWrites=true&w=majority");
 const DB=mongoose.connection;
 DB.on('error',err=>{logError(err)});
 DB.once('open',()=>{

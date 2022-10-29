585f229760b85a903e195c354635de200ed0e816
Pushkar gupta <pushkargupta808@gmail.com>
2022-10-29 15:48:00 +0530

    s

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 // mongoose.connect("mongodb://localhost:27017/PlacementCell");
 
-mongoose.connect("mongodb+srv://pushkar:EWY4RKwBpB8txyqx@cluster0.1hfkic2.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://pushkar:EWY4RKwBpB8txyqx@cluster0.1hfkic2.mongodb.net/?retryWrites=true&w=majority/test");
 const DB=mongoose.connection;
 DB.on('error',err=>{logError(err)});
 DB.once('open',()=>{
